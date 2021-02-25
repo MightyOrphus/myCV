0c3e676fd82be54aa0781c92685311fcb812a617
orphus <patwonggtr@gmail.com>
2021-02-26 00:02:59 +0700

    typo

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -10,7 +10,7 @@
     <div class="about-me-section-container">
       <div id="header">CERTIFICATES</div>
       <div>
-        Some badge or certificate pictures should be here but somehow can't
+        Some badge or certificate pictures should be here but somehow I can't
         gather them...
       </div>
     </div>
