ae15459159441ae2855c9bc5ed5df8305b7386c7
orphus <patwonggtr@gmail.com>
2021-03-07 00:10:01 +0700

    add loader for the whole page

--- src/App.vue
+++ src/App.vue
@@ -22,28 +22,13 @@ export default {
     AboutMe,
     Journey,
   },
+  mounted: function() {
+    document.body.removeChild(document.getElementById("loaderWrapper"));
+  },
 };
 </script>
 
 <style>
-:root {
-  /* --color-1: #c2dfe1;
-  --color-2: #53adac;
-  --color-3: #006370;
-  --color-4: #ef9a9e;
-  --color-5: #efd9d9; */
-
-  --color-1: #2f4159;
-  --color-2: #6f87a6;
-  --color-3: #5e728c;
-  --color-4: #1a1f26;
-  --color-5: #223440;
-}
-html,
-body {
-  /* background-color: var(--color-1); */
-  background-color: var(--color-4);
-}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
